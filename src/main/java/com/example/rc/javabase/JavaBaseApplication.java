63f1c537555769d1c4ab8800a8c674b3a18b7d78
zhangruchao <1011044055>
2019-09-09 15:26:45 +0800

    first

---
+++ src/main/java/com/example/rc/javabase/JavaBaseApplication.java
@@ -0,0 +1,13 @@
+package com.example.rc.javabase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaBaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaBaseApplication.class, args);
+	}
+
+}
