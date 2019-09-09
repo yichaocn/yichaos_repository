188b400f5734b729194a0857acc5fd950f287cf6
zhangruchao <1011044055>
2019-09-09 16:16:54 +0800

    test1

--- src/main/java/com/example/rc/javabase/JavaBaseApplication.java
+++ src/main/java/com/example/rc/javabase/JavaBaseApplication.java
@@ -2,7 +2,11 @@ package com.example.rc.javabase;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+/*
+* 张汝超
+* 2019-9-9 16:16:32
+*
+* */
 @SpringBootApplication
 public class JavaBaseApplication {
 
